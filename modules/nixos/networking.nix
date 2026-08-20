10f585e862dc681daac721221a9e976e4faf1a88
gitPrivate.name <gitPrivate.email>
2026-08-20 06:49:51 +0200

    nerwork

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -3,6 +3,9 @@
 {
   networking.networkmanager.enable = true;
   networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.extraHosts = ''
+    127.0.0.1 searxng.local
+  '';
 
   services.openssh = {
     enable = true;
