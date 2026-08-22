3d8c90b15eeff1700ce9ee7e0e119d4e67bc6385
Holoibke <azkozica@disroot.org>
2026-08-22 02:27:03 +0200

    g

--- home/holibken/programs/getmail.nix
+++ home/holibken/programs/getmail.nix
@@ -2,6 +2,9 @@
 
 let
   mailPrivate = import /home/holibken/.config/credentials/mail;
+  extractMailPassword = pkgs.writeShellScript "extract-mail-password" ''
+    sed -n 's/^[[:space:]]*password = "\(.*\)";.*/\1/p' "$1"
+  '';
 in
 {
   home.packages = [ pkgs.getmail6 ];
@@ -10,26 +13,30 @@ in
     "d %h/Mail 0700 - - - -"
     "d %h/.getmail 0700 - - - -"
   ];
+
   xdg.configFile."getmail/getmailrc".text = ''
     [retriever]
     type = SimplePOP3SSLRetriever
     server = disroot.org
     port = 995
     username = ${mailPrivate.username}
-    password_command = password_command = ("${extractMailPassword}", "/home/holibken/.config/credentials/mail")
+    password_command = ("${extractMailPassword}", "/home/holibken/.config/credentials/mail")
 
     [destination]
     type = Maildir
     path = ~/Mail/disroot/
 
     [options]
-    # flip delete to true once you trust it, if you want disroots own inbox to empty out as mail lands locally
+    # Starting conservative: nothing gets deleted from the server until
+    # you've confirmed this actually works reliably. Flip delete to true
+    # once you trust it, if you want Disroot's own inbox to empty out as
+    # mail lands locally.
     read_all = true
     delete = false
     verbose = 1
     message_log = ~/.getmail/disroot.log
   '';
-
+  
   systemd.user.services.getmail-disroot = {
     Unit.Description = "Fetch Disroot mail via getmail6";
     Service = {
