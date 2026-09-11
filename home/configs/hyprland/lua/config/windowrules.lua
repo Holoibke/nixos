d93ebbb8f89b759dc16ad6d520e2ca71c8c5ef54
Holoibke <azkozica@disroot.org>
2026-09-11 21:35:48 +0200

    grzyb

--- home/configs/hyprland/lua/config/windowrules.lua
+++ home/configs/hyprland/lua/config/windowrules.lua
@@ -10,37 +10,37 @@ hl.window_rule({
 })
 
 -- Gaming
-local gamingApps = "^(steam_app.*|gamescope)$"
-local gamingWorkspace = "name:gaming"
+-- local gamingApps = "^(steam_app.*|gamescope)$"
+-- local gamingWorkspace = "name:gaming"
 
-hl.window_rule({ match = { content = "game" }, workspace = gamingWorkspace })
-hl.window_rule({ match = { xdg_tag = "^(.+game.+)$" }, workspace = gamingWorkspace, fullscreen_state = 2, content = "game", sync_fullscreen = true })
-hl.window_rule({ match = { class = gamingApps }, workspace = gamingWorkspace })
-hl.window_rule({ match = { class = "^(steam)$", title = "^(Friends List)$" }, float = true })
-hl.window_rule({ match = { class = "^(steam)$", title = "^(Launching\\.{3})$" }, float = true, center = true, workspace = gamingWorkspace })
-hl.window_rule({
-    match = {
-        class         = gamingApps,
-        title         = "^(.+)$",
-        initial_title = "negative:^(.+\\.home\\.+)$",
-    },
-    content          = "game",
-    decorate         = false,
-    fullscreen_state = 2,
-    size             = { "monitor_w", "monitor_h" },
-    sync_fullscreen  = true,
-})
-hl.window_rule({
-    match = {
-        class        = "^(steam_app.*)$",
-        initial_title = "^$",
-    },
-    center           = true,
-    float            = true,
-    fullscreen       = false,
-    fullscreen_state = 0,
-    workspace        = gamingWorkspace,
-})
+-- hl.window_rule({ match = { content = "game" }, workspace = gamingWorkspace })
+-- hl.window_rule({ match = { xdg_tag = "^(.+game.+)$" }, workspace = gamingWorkspace, fullscreen_state = 2, content = "game", sync_fullscreen = true })
+-- hl.window_rule({ match = { class = gamingApps }, workspace = gamingWorkspace })
+-- hl.window_rule({ match = { class = "^(steam)$", title = "^(Friends List)$" }, float = true })
+-- hl.window_rule({ match = { class = "^(steam)$", title = "^(Launching\\.{3})$" }, float = true, center = true, workspace = gamingWorkspace })
+-- hl.window_rule({
+--     match = {
+--         class         = gamingApps,
+--         title         = "^(.+)$",
+--         initial_title = "negative:^(.+\\.home\\.+)$",
+--     },
+--     content          = "game",
+--     decorate         = false,
+--     fullscreen_state = 2,
+--     size             = { "monitor_w", "monitor_h" },
+--     sync_fullscreen  = true,
+-- })
+-- hl.window_rule({
+--     match = {
+--         class        = "^(steam_app.*)$",
+--         initial_title = "^$",
+--     },
+--     center           = true,
+--     float            = true,
+--     fullscreen       = false,
+--     fullscreen_state = 0,
+--     workspace        = gamingWorkspace,
+-- })
 
 -- Apps
 hl.window_rule({ match = { class = "^(.+\\.exe)$", float = true }, monitor = PRIMARY_MONITOR, center = true, fullscreen_state = 0 })
--- home/configs/zsh/default.nix
+++ home/configs/zsh/default.nix
@@ -22,6 +22,7 @@
 
     shellAliases = {
       nrs = "sudo nixos-rebuild switch --flake /etc/nixos#Mieszko-II-Lambert --impure";
+      nrf = "sudo nixos-rebuild switch --flake path:/etc/nixos#Mieszko-II-Lambert --impure";
       nrb = "sudo nixos-rebuild build --flake /etc/nixos#Mieszko-II-Lambert --impure";
       ncg = "nix-collect-garbage -d";
       nsp = "nix-shell -p";
